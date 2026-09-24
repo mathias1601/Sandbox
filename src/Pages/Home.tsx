cf7391ff7b07dffdb164c6d7e24b0a1af8ed0468
Mathias Knudsen <mathias.xinyu.yang@gmail.com>
2026-09-24 16:23:09 +0200

    bug: fix typo

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,7 @@ import SandcastleCustomizer from "../components/sandcastle/SandcastleCustomizer"
 
 function Home() {
   const titleMessage = "Hallo!"
-  const mainMessage = "Jeg er en master-student på mitt fjerde år på UiO, og studerer Programmering og Systemarkitektur (PROSA). Skjekk ut nettsiden min for å bli litt bedre kjent med meg!"
+  const mainMessage = "Jeg er en master-student på mitt fjerde år på UiO, og studerer Programmering og Systemarkitektur (PROSA). Sjekk ut nettsiden min for å bli litt bedre kjent med meg!"
 
   return (
     <div>
@@ -11,7 +11,7 @@ function Home() {
           <h1>{titleMessage}</h1>
         <div className='introText'>
           <p>{mainMessage}</p>
-          <p>Jeg navnga nettsiden min "Sandbox" fordi den skulle operere lignende en vanlig sandkasse i virkeligheten. Et lite sted hvor jeg kan vise frem diverse prosjekter, mens jeg sakte men sikkert lærer meg å lage sandslottene (prosjektene) litt penere for hver gang</p>
+          <p>Jeg navnga nettsiden min "Sandbox" fordi den skulle operere lignende en vanlig sandkasse i virkeligheten. Et lite sted hvor jeg kan vise frem diverse prosjekter, mens jeg sakte men sikkert lærer meg å lage sandslottene (prosjektene) litt penere for hver gang.</p>
         </div>
       </div>
       <SandcastleCustomizer />
